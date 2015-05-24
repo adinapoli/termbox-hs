206fc3e30f282c11fab10c7fc2bcce08a3c1861d
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2015-05-24 18:51:17 +0200

    cleaning

--- src/Termbox.c
+++
@@ -1,9 +0,0 @@
-
-#include <sys/ioctl.h>
-
-#include "wrapper.h"
-
-int inline_c_0_27845636a9593d6afbe261949c7f56b1c314bfeb(Win_Size * wsPtr_inline_c_0) {
-return ( ioctl(0, TIOCGWINSZ, wsPtr_inline_c_0) );
-}
-
